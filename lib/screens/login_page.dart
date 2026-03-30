f851991c78b46e5077d4b80aea961b5579491ff3
Aldo <aldopanduws@gmail.com>
2026-03-30 10:17:30 +0700

    mengganti api

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -24,7 +24,7 @@ class _LoginPageState extends State<LoginPage> {
       isLoading = true;
     });
 
-    final url = Uri.parse("http://127.0.0.1:8000/api/login");
+    final url = Uri.parse("http://172.16.106.79:8000/api/login");
 
     final response = await http.post(
       url,
